0f6d886d912575bce8b4bc244324086906e222f7
gavin rees <gavin.j.rees@gmail.com>
2015-08-26 15:43:24 +0000

    Initial commit

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if [ "${1:0:1}" != '-' ]; then
+	exec "$@"
+fi
+
+exec java -jar lib/sonar-application-${SONARQUBE_VERSION}.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="${SONARQUBE_JDBC_USERNAME}" \
+  -Dsonar.jdbc.password="${SONARQUBE_JDBC_PASSWORD}" \
+  -Dsonar.jdbc.url="${SONARQUBE_JDBC_URL}" \
+  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" "$@"
